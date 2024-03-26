8c5aea9e9698a86687be65ed7201237c505f6570
saqlain <saqlainali.saqlainali08@gmail.com>
2024-03-27 00:03:26 +0500

    pushing calculator first time

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([
+    { message: "enter your's first number : ", type: "number", name: "firstNumber" },
+    { message: "enter your's second number :", type: "number", name: "secondNumber" },
+    { message: "choose differt operator given in below :", type: "list", name: "operator", choices: ["addition", "subtraction", "multiplication", "division"] },
+]);
+if (ans.operator === "addition") {
+    console.log(ans.firstNumber + ans.secondNumber);
+}
+else if (ans.operator === "subtraction") {
+    console.log(ans.firstNumber - ans.secondNumber);
+}
+else if (ans.operator === "multiplication") {
+    console.log(ans.firstNumber * ans.secondNumber);
+}
+else if (ans.operator === "division") {
+    console.log(ans.firstNumber / ans.secondNumber);
+}
+else {
+    console.log("invalid input");
+}
